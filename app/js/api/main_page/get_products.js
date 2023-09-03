ce4dcc93b2faf70c26e308e8a2a1bb3762014c8b
GederSet <grischastulov10@gmail.com>
2023-09-03 15:59:08 +0300

    create hide text in main v1

--- app/js/api/main_page/get_products.js
+++ app/js/api/main_page/get_products.js
@@ -34,16 +34,20 @@ export async function showProducts(minPrice, maxPrice, gender, strapMaterial, st
             productsBody.innerHTML +=
                 `
                 <div class="products__card" value="${products.id}">
-                    <div class="products__image">
-                        <img src="${products.url_image}" alt="${products.name}">
-                    </div>
-                    <div class="products__rows">
-                        <div class="products__page">
-                            <div class="products__name">${products.name}</div>
-                            <div class="products__price">${products.price} ₽</div>
+                    <div class="products__shell-image">
+                        <div class="products__image">
+                            <img src="${products.url_image}" alt="${products.name}">
                         </div>
-                        <div class="products__page">
-                            <div class="products__basket _icon-basket-add _schowIcon"></div>
+                    </div>
+                    <div class="products__shell-info">
+                        <div class="products__rows">
+                            <div class="products__page">
+                                <div class="products__name">${products.name}</div>
+                                <div class="products__price">${products.price} ₽</div>
+                            </div>
+                            <div class="products__page">
+                                <div class="products__basket _icon-basket-add _schowIcon"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
