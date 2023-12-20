56985acd2bda46fe4091f38e5cbcc217f7cb304d
GederSet <grischastulov10@gmail.com>
2023-12-20 23:26:32 +0300

    removing unnecessary files

--- api/delivery/change_product_status.php
+++ api/delivery/change_product_status.php
@@ -6,12 +6,10 @@
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    // header("Content-Type: text/event-stream"); 
 
 
     require_once '../config/Database.php';
     require_once '../objects/Delivery.php';
-    // require '../../vendor/autoload.php';
 
     $database = new Database();
     $conn = $database->getConnection();
@@ -22,18 +20,3 @@
     $id = implode(",", $data->product_id);
 
     $delivery->changeProductStatus($id, $user_id);
-
-
-    // $loop = \React\EventLoop\Factory::create();
-
-    // $status_product = 1;
-
-    // $timer = $loop->addPeriodicTimer(5, function () use ($loop, $user_id, $product_id, &$status_product, &$timer) {
-    //     $delivery->changeProductStatus($user_id, $product_id, $status_product);
-    //     $status_product++;
-    //     if ($status_product == 6) {
-    //         $loop->cancelTimer($timer);
-    //     }
-    // });
-
-    // $loop->run();
